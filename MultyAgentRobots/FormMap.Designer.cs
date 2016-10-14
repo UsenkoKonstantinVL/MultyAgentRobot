2dd57ff49e9b93112cb4c92273f3ebd3d1e9efd0
KonstantinPC <kos16@mail.ru>
2016-10-14 22:15:20 +0300

    Добавил Класс для работы с редактором карты. Рисование сетки.

--- MultyAgentRobots/FormMap.Designer.cs
+++ MultyAgentRobots/FormMap.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(371, 316);
+            this.ClientSize = new System.Drawing.Size(385, 327);
             this.Name = "FormMap";
             this.Text = "FormMap";
             this.ResumeLayout(false);
---
+++ MultyAgentRobots/MainClasses/MapEditManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace MultyAgentRobots.MainClasses
+{
+    public class MapEditManager
+    {
+        #region Constant
+        public static int mapHeight = 500;
+        public static int mapWidth = 500;
+
+        public static int Cell = 5;
+        #endregion
+        Bitmap image;
+
+        public MapEditManager()
+        {
+            image = new Bitmap(mapWidth, mapHeight);
+        }
+
+
+        public Bitmap DrawCell(int x, int y, Occyped occyped, int stracheCell)
+        {
+
+            return DrawGrid();
+        }
+
+        private Bitmap DrawGrid()
+        {
+            Bitmap _image = (Bitmap)image.Clone();
+
+            Graphics g;
+
+            g = Graphics.FromImage(_image);
+
+            Pen myPen = new Pen(Color.Gray);
+
+            myPen.Width = 1;
+
+            int h = mapHeight / Cell;
+
+            for (int i = 1; i < (h - 1); i++)
+            {
+                int x1, y1;
+                int x2, y2;
+
+
+                y1 = 0;
+                y2 = mapHeight - 1;
+
+                x1 = (i * Cell) - 1;
+                x2 = x1;
+
+                g.DrawLine(myPen, x1, y1, x2, y2);
+            }
+
+            for (int i = 1; i < (h - 1); i++)
+            {
+                int x1, y1;
+                int x2, y2;
+
+
+                y1 = (i * Cell) - 1;
+                y2 = y1;
+
+                x1 = 0;
+                x2 = mapWidth - 1;
+
+                g.DrawLine(myPen, x1, y1, x2, y2);
+            }
+
+            return _image;
+        }
+    }
+
+
+    public enum Occyped
+    {
+        Occyped, NoOccyped
+    }
+}
